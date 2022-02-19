26bea31bb81e7cba26f4ce1589574e40a1dd0aee
Joseph Soria <josephsoria6@gmail.com>
2022-02-19 14:05:31 -0600

    Added id to InputContainer, imported App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import "./App.css";
 import InputContainer from "./components/InputContainer";
 import OutputContainer from "./components/OutputContainer";
+import "./App.css";
+
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <h1 className="text-center">Password Generator</h1>
       </header>
       <div className="row justify-content-center">
-        <div className="col-md-4">
+        <div className="col-md-4" id="input-container">
           <InputContainer />
         </div>
         <div className="col-md-4">
