0728ba524e7844a6f61efb21b57134225685d7c0
Joseph Soria <josephsoria6@gmail.com>
2022-02-19 14:06:40 -0600

    Made form

--- src/components/InputContainer.js
+++ src/components/InputContainer.js
@@ -1,12 +1,106 @@
 import React from "react";
+import options from "../helper/options";
+import "./InputContainer.css";
 
 class InputContainer extends React.Component {
+  mapCharLengthOptions() {
+    return options.charLength.map((value) => {
+      return (
+        <option value={value} key={value}>
+          {value}
+        </option>
+      );
+    });
+  }
+
+  mapNumOfPasswords() {
+    return options.numOfPasswords.map((value) => {
+      return (
+        <option value={value} key={value}>
+          {value}
+        </option>
+      );
+    });
+  }
+
   render() {
     return (
-      <div>
-        <h1>This is the input container</h1>
-      </div>
-    )
+      <React.Fragment>
+        <div className="row">
+          <h4 className="text-center" id="options-header">
+            Select your options
+          </h4>
+        </div>
+        <div className="row">
+          <form>
+            <label htmlFor="passwordLength">Password Character Length: </label>
+            <select name="passwordLength" id="passwordLength">
+              {this.mapCharLengthOptions()}
+            </select>
+            <div className="form-check form-switch">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="lowercase"
+              />
+              <label className="form-check-label" htmlFor="lowercase">
+                Lowercase Characters [a-z]
+              </label>
+            </div>
+            <div className="form-check form-switch">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="uppercase"
+              />
+              <label className="form-check-label" htmlFor="uppercase">
+              Uppercase Characters [A-Z]
+              </label>
+            </div>
+            <div className="form-check form-switch">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="symbols"
+              />
+              <label className="form-check-label" htmlFor="symbols">
+              Symbols [!@#$%^&*()]
+              </label>
+            </div>
+            <div className="form-check form-switch">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="numbers"
+              />
+              <label className="form-check-label" htmlFor="numbers">
+              Numbers [0-9]
+              </label>
+            </div>
+            <div className="form-check form-switch">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="unique"
+              />
+              <label className="form-check-label" htmlFor="unique">
+              Unique Values [No Duplicates]
+              </label>
+            </div>
+            <label htmlFor="numOfPasswords">Number of Passwords: </label>
+            <select name="numOfPasswords" id="numOfPasswords">
+              {this.mapNumOfPasswords()}
+            </select>
+            <button className="btn btn-primary d-block m-auto">Submit</button>
+          </form>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
