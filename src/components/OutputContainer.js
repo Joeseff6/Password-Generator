be9c55165056b419471c0f3c8845c8d847acd844
Joseph Soria <josephsoria6@gmail.com>
2022-02-19 12:08:50 -0600

    Made OutputContainer component

---
+++ src/components/OutputContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class OutputContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the output container</h1>
+      </div>
+    )
+  }
+}
+
+export default OutputContainer;
