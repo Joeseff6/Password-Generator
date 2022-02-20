d9405e270afe28a50b34b39739d7b55409a69640
Joseph Soria <josephsoria6@gmail.com>
2022-02-20 17:11:28 -0600

    Added h5 tag, passed arrow function for clicking passwords

--- src/components/OutputContainer.js
+++ src/components/OutputContainer.js
@@ -2,21 +2,18 @@ import React from "react";
 import "./OutputContainer.css";
 
 class OutputContainer extends React.Component {
-  onPasswordClick = (e) => {
-    navigator.clipboard.writeText(e.target.innerText);
-  }
-
   render() {
     return (
       <React.Fragment>
         <div className="row">
-          <h4 className="d-flex align-items-center justify-content-center mb-5" id="output-header">
+          <h4 className="d-flex align-items-center justify-content-center mb-3" id="output-header">
             Generated Passwords
           </h4>
+          {this.props.generatedPasswords.length ? <h5 className="text-center" style={{ color: "white"}}>Click on a password to copy it</h5> : null};
           {this.props.generatedPasswords.map((password) => {
             return (
               <div className="row justify-content-center my-2" key={password}>
-                <button className="generated-password-block col-md-6 text-center" onClick={this.onPasswordClick}>
+                <button className="generated-password-block col-md-6 text-center" onClick={(e) => navigator.clipboard.writeText(e.target.innerText)}>
                   {password}
                 </button>
               </div>
